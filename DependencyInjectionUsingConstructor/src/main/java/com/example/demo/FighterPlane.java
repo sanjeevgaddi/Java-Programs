7f2cae52bf8f3dcb9933ac7209e43f85c7b101bc
sanjeevgaddi <sanjeevgaddi7@gmail.com>
2023-11-23 23:59:02 +0530

    #initial commit

---
+++ DependencyInjectionUsingConstructor/src/main/java/com/example/demo/FighterPlane.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class FighterPlane implements Plane {
+
+	@Override
+	public String takeoff() {
+		// TODO Auto-generated method stub
+		return "Fighterplane TakeOff on a small Runway:-)))";
+	}
+
+}
---
+++ DependencyInjectionUsingConstructor/src/main/java/com/example/demo/PassengerPlane.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class PassengerPlane implements Plane {
+
+	@Override
+	public String takeoff() {
+		// TODO Auto-generated method stub
+		return "PassengerPlane Takeoff on Medium Sized Runway:-))))";
+	}
+
+}
