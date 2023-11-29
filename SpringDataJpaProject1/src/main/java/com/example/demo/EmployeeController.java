7b80afb9b3484fa8975d8aa7294ab516a4c334f7
sanjeevgaddi <sanjeevgaddi7@gmail.com>
2023-11-30 00:18:42 +0530

    #initial commit

---
+++ SpringDataJpaProject1/src/main/java/com/example/demo/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	EmployeeRepository  ref;
+	
+	@Autowired
+	public EmployeeController(EmployeeRepository ref) {
+		this.ref=ref;
+		
+	}
+	
+	@PostMapping("/saveemployee")
+	public Employee saveEmployee(@RequestBody Employee e)
+	{
+		return ref.save(e);
+	}
+
+}
