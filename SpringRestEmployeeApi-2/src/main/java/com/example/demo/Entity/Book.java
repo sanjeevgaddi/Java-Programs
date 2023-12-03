2fc8e14bfc763b4c99f8591ff897d3f2ba70d253
sanjeevgaddi <sanjeevgaddi7@gmail.com>
2023-12-03 11:08:50 +0530

    #initial commit

---
+++ SpringRestEmployeeApi-2/src/main/java/com/example/demo/Entity/Book.java
@@ -0,0 +1,73 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="book")
+public class Book {
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	int id;
+	@Column(name="title")
+	String title;
+	@Column(name="author")
+	String author;
+	@Column(name="price")
+	int price;
+	
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Book(String title, String author, int price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Books [id=" + id + ", title=" + title + ", author=" + author + ", price=" + price + "]";
+	}
+	
+
+}
